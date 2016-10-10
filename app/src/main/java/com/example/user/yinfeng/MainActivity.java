a3eb13343ab8e49c081c9641688238b65100453b
walkermannum1 <2469377884@qq.com>
2016-10-10 19:49:47 +0800

    Add libs and layout.

--- app/src/main/java/com/example/user/yinfeng/MainActivity.java
+++ app/src/main/java/com/example/user/yinfeng/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.user.yinfeng;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- app/src/main/java/com/example/user/yinfeng/MonitorService.java
+++ app/src/main/java/com/example/user/yinfeng/MonitorService.java
@@ -1,8 +1,18 @@
 package com.example.user.yinfeng;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
 /**
  * Created by user on 2016/10/10.
  */
 
-public class MonitorService {
+public class MonitorService extends Service{
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
