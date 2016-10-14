9871bcd386377233f8ea271ef4b3381e4ad7d843
walkermannum1 <2469377884@qq.com>
2016-10-14 17:38:21 +0800

    Fragment

--- app/src/main/java/com/example/user/yinfeng/ui/fragment/HuodongFragment.java
+++ app/src/main/java/com/example/user/yinfeng/ui/fragment/HuodongFragment.java
@@ -2,6 +2,15 @@ package com.example.user.yinfeng.ui.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.user.yinfeng.R;
+
+import java.security.PublicKey;
+
+import static com.baidu.location.h.j.P;
 
 /**
  * Created by user on 2016/10/13.
@@ -15,5 +24,21 @@ public class HuodongFragment extends Fragment{
         HuodongFragment fragment = new HuodongFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public HuodongFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_huodong, container, false);
+        Bundle bundle = getArguments();
+        return view;
     }
 }
